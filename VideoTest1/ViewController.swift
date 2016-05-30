00d7f83c00b3405572700bb242430a5772566a95
lmx0412 <LiuMengxuan@liumengxuandeimac.local>
2016-05-30 20:34:12 +0000

    Initial Commit

---
+++ VideoTest1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoTest1
+//
+//  Created by 刘孟轩 on 16/5/30.
+//  Copyright © 2016年 刘孟轩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
